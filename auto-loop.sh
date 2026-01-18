36920ddd7425711f801c3d868cc55b8d9121c0f4
JT Archie <hello2025+github@jtarchie.com>
2026-01-18 00:25:56 -0700

    shfmt

--- auto-loop.sh
+++ auto-loop.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-m=$1 p=$2; shift 2; [[ $1 == "--" ]] && shift
-i=0; while read -r s; do echo "[$((++i))] $s"; copilot --model "$m" --prompt "$p $s" "${@:---allow-all-tools --disallow-temp-dir}" --silent; done
+m=$1 p=$2 i=0
+shift 2
+[[ $1 == "--" ]] && shift
+while read -r s; do
+	echo "[$((++i))] $s"
+	copilot --model "$m" --prompt "$p $s" "${@:---allow-all-tools --disallow-temp-dir}" --silent
+done
